6b8f6909bca3fd328e9ab58956398c12930f82f0
SpiegelV <leon.schuessler@freenet.de>
2022-04-21 22:27:14 +0200

    Initial Commit.

---
+++ src/main/java/htw/berlin/webtech/matefinder/MateFinderApplication.java
@@ -0,0 +1,13 @@
+package htw.berlin.webtech.matefinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MateFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MateFinderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/htw/berlin/webtech/matefinder/MateFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package htw.berlin.webtech.matefinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MateFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
