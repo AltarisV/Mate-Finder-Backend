2d8d64a430815ae341faa521323c5d84348db9e8
SpiegelV <leon.schuessler@freenet.de>
2022-06-18 00:41:35 +0200

    Small Changes for Frontend Communication

--- src/main/java/htw/berlin/webtech/matefinder/config/WebConfig.java
+++ src/main/java/htw/berlin/webtech/matefinder/config/WebConfig.java
@@ -1,6 +1,8 @@
 package htw.berlin.webtech.matefinder.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -10,8 +12,27 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
+    /**
+     * Configure "global" cross origin request processing. The configured CORS
+     * mappings apply to annotated controllers, functional endpoints, and static
+     * resources.
+     * <p>Annotated controllers can further declare more fine-grained config via
+     * {@link CrossOrigin @CrossOrigin}.
+     * In such cases "global" CORS configuration declared here is
+     * {@link CorsConfiguration#combine(CorsConfiguration) combined}
+     * with local CORS configuration defined on a controller method.
+     *
+     * @param registry Does things
+     * @see CorsRegistry
+     * @see CorsConfiguration#combine(CorsConfiguration)
+     * @since 4.2
+     */
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+        registry.addMapping("/**")
+                .allowedMethods("*")
+                .allowedOrigins("http://localhost:8080",
+                                "https://drinkmate-finder.herokuapp.com"
+                );
     }
 }
