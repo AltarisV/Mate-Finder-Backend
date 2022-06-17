43eebaaf1eea946bec3f1124d7ca71f86b630c94
SpiegelV <leon.schuessler@freenet.de>
2022-06-17 23:16:51 +0200

    Small Changes for Frontend Communication

--- src/main/java/htw/berlin/webtech/matefinder/config/WebConfig.java
+++ src/main/java/htw/berlin/webtech/matefinder/config/WebConfig.java
@@ -12,7 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
+        registry.addMapping("/**")
                 .allowedMethods("*")
                 .allowedOrigins(
                         "http://localhost:8080",
